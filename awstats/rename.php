59d39e97ecc6c8758012e40b8a8210adec4da4e7
culdev <lucas03093@gmail.com>
2012-10-25 16:58:37 +0200

    Added awstats scripts

---
+++ awstats/rename.php
@@ -0,0 +1,23 @@
+<?php
+// CLI
+if(count($argv) <= 1)
+    die("You must supply a dir argument, e.g php $argv[0] testfolder\n");
+
+$dir = $argv[1];
+if($handle = opendir($dir))
+{
+	while(false !== ($file = readdir($handle)))
+	{
+		if($file != "." && $file != "..")
+		{
+			$pathinfo = pathinfo($file);
+            
+            if(!strpos($file, $dir))
+            {
+                $newname = $pathinfo['filename'].'.'.$dir.'.'.$pathinfo['extension'];
+                rename($dir.DIRECTORY_SEPARATOR.$file, $dir.DIRECTORY_SEPARATOR.$newname);
+            }
+		}
+	}
+}
+?>
