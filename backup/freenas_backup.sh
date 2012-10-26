e7f78bb5d30464fd1a415bca0c118115fb05295f
culdev <lucas03093@gmail.com>
2012-10-26 13:22:24 +0200

    FreeNAS backup

---
+++ backup/freenas_backup.sh
@@ -0,0 +1,7 @@
+﻿DATE=`date '+%Y%m%d'`
+
+# Remove old files
+find /path/to/backup/*freenas* -mtime +15 -exec rm {} \;
+
+# freenas data
+tar cfz /path/to/backup/freenas.$DATE.tar /data/freenas-v1.db
