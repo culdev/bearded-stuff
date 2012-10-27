afd888e1148bd5352e0f455f48c4495b7421901a
culdev <lucas03093@gmail.com>
2012-10-28 02:26:36 +0300

    Mount dropbox if necessary.

--- backup/dropbox_backup_master.sh
+++ backup/dropbox_backup_master.sh
@@ -4,12 +4,22 @@
 # Settings
 PASSPHRASE=""
 ARCHIVES="/var/archives"
-DROPBOX="/home/dropbox/Dropbox/_Backups"
+BASEDROPBOX="/home/dropbox/Dropbox"
+DROPBOX=$BASEDROPBOX"/_Backups"
 
 DATE=`date '+%Y%m%d'`
 
 MYSQLLOCAL=$ARCHIVES"/Hostname-mysql"
 MYSQLDROPBOX=$DROPBOX"/mysql/Hostname-mysql"
+
+# Mount $BASEDROPBOX if necessary
+# This should be disabled if $BASEDROPBOX is always mounted
+if ! grep -q $BASEDROPBOX /proc/mounts ; then
+    if ! mount $BASEDROPBOX ; then
+        echo "Failed to mount "$BASEDROPBOX". Exiting."
+        exit 1
+    fi
+fi
 
 # Remove previous backups
 find $DROPBOX/mysql/*mysql* -exec rm {} \;
