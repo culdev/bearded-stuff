a2317c1313c5c3d170b36c3a4fa4008c0299f568
culdev <lucas03093@gmail.com>
2012-10-28 02:25:29 +0300

    Mount dropbox if necessary.

--- backup/dropbox_backup_incremental.sh
+++ backup/dropbox_backup_incremental.sh
@@ -4,12 +4,22 @@
 # Settings
 PASSPHRASE=""
 ARCHIVES="/var/archives"
-DROPBOX="/home/dropbox/Dropbox/_Backups"
+BASEDROPBOX="/home/dropbox/Dropbox"
+DROPBOX=$BASEDROPBOX"/_Backups"
 
 DATE=`date '+%Y%m%d'`
 
 MYSQLLOCAL=$ARCHIVES"/Hostname-mysql"
 MYSQLDROPBOX=$DROPBOX"/mysql/Hostname-mysql"
+
+# Mount $BASEDROPBOX if necessary
+# This should be disabled if $BASEDROPBOX is always mounted
+if ! grep -q $BASEDROPBOX /proc/mounts ; then
+    if ! mount $BASEDROPBOX ; then
+        echo "Failed to mount "$BASEDROPBOX". Exiting."
+        exit 1
+    fi
+fi
 
 # Copy todays backups
 openssl des3 -salt -k $PASSPHRASE -in $MYSQLLOCAL.$DATE.tar.gz -out $MYSQLDROPBOX.$DATE.tar.gz.encrypted
