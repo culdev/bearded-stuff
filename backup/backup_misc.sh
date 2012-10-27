efbe7122ecac66bb1d6c829e9c87922b29fafc89
culdev <lucas03093@gmail.com>
2012-10-28 02:33:20 +0300

    Mount check.

--- backup/backup_misc.sh
+++ backup/backup_misc.sh
@@ -1,9 +1,22 @@
 #!/bin/sh
 
+# Settings
+BASETARGET="/path/to/backup"
+TARGETDIR=$BASETARGET"/Hostname"
+
 DATE=`date '+%Y%m%d'`
 
+# Mount $BASETARGET if necessary
+# This should be disabled if $BASETARGET is always mounted
+if ! grep -q $BASETARGET /proc/mounts ; then
+    if ! mount $BASETARGET ; then
+        echo "Failed to mount "$BASETARGET". Exiting."
+        exit 1
+    fi
+fi
+
 # Remove old files
-find /path/to/backup/*etc* -mtime +15 -exec rm {} \;
+find $TARGETDIR/*etc* -mtime +15 -exec rm {} \;
 
 # /etc
-tar cvfz /path/to/backup/etc.$DATE.tar.gz /etc
+tar cvfz $TARGETDIR/etc.$DATE.tar.gz /etc
