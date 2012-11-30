a0073e1935a9287afb296b193d16a7967e58e937
culdev <lucas03093@gmail.com>
2012-11-30 21:35:12 +0100

    Fixed tab.

--- init_vm.sh
+++ init_vm.sh
@@ -67,7 +67,7 @@ SHELL=/bin/sh
 
 #minute (0-59)
 #|	hour (0-23)
-#|  |    day of the month (1-31)
+#|	|   day of the month (1-31)
 #|	|	|   month of the year (1-12 or Jan-Dec)
 #|	|	|	|   day of the week (0-6 with 0=Sun or Sun-Sat)
 #|	|	|	|	|	commands
