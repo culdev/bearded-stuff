ab35d15f3f3e669d71f2f94d0bdb4f2028039b89
culdev <lucas03093@gmail.com>
2012-12-24 17:20:37 +0100

    Css style for system information. Added hostname, debian version, kernel information and swap information.

--- raspberrypi/mailstatus.sh
+++ raspberrypi/mailstatus.sh
@@ -3,7 +3,20 @@ TMP=/tmp/status
 MAIL="some@mail.com"
 SUBJECT="Pi Status"
 
-echo "<html><head></head><body><p>" > $TMP
+echo "<html><head><style type=\"text/css\">
+#wrap {
+    width: 100%;
+    position: relative;
+}
+#one {
+    float:left;
+    width: 20%;
+}
+#two {
+    float:right;
+    width: 80%;
+}
+</style></head><body><p>" > $TMP
 
 # Get cpu information
 cpucurfreq=$(cat /sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_cur_freq)
@@ -13,18 +26,36 @@ cpugovernor=$(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor)
 cputemp=$(cat /sys/class/thermal/thermal_zone0/temp)
 cputempthrottle=$(cat /sys/class/thermal/thermal_zone0/trip_point_0_temp)
 cpuusage=$(eval $(awk '/^cpu /{print "previdle=" $5 "; prevtotal=" $2+$3+$4+$5 }' /proc/stat); sleep 0.4; eval $(awk '/^cpu /{print "idle=" $5 "; total=" $2+$3+$4+$5 }' /proc/stat); intervaltotal=$((total-${prevtotal:-0})); echo "$((100*( (intervaltotal) - ($idle-${previdle:-0}) ) / (intervaltotal) ))")"%"
-uptime=$(uptime)
 
-echo "<b>Information:</b><pre>
-CPU governor:		  $cpugovernor
-CPU frequency cur:	  $(($cpucurfreq/1000)) MHz
-CPU frequency min:	  $(($cpuminfreq/1000)) MHz
-CPU frequency max:	  $(($cpumaxfreq/1000)) MHz
-CPU temperature:	  $(($cputemp/1000))C
-CPU throttle temperature: $(($cputempthrottle/1000))C
-CPU usage:		  $cpuusage
-Uptime:			 $uptime
-</pre>" >> $TMP
+echo "<b>System Information:</b>
+<div id=\"wrap\">
+    <div id=\"one\">
+        CPU governor:<br>
+        CPU frequency cur:<br>
+        CPU frequency min:<br>
+        CPU frequency max:<br>
+        CPU temperature:<br>
+        CPU throttle temperature:<br>
+        CPU usage:<br>
+        Uptime:<br>
+        Hostname:<br>
+        Debian version:<br>
+        Kernel:<br>
+    </div>
+    <div id=\"two\">
+        $cpugovernor<br>
+        $(($cpucurfreq/1000)) MHz<br>
+        $(($cpuminfreq/1000)) MHz<br>
+        $(($cpumaxfreq/1000)) MHz<br>
+        $(($cputemp/1000))C<br>
+        $(($cputempthrottle/1000))C<br>
+        $cpuusage<br>
+        $(uptime)<br>
+        $(hostname)<br>
+        $(cat /etc/debian_version)<br>
+        $(uname -a)<br>
+    </div>
+</div>" >> $TMP
 
 # Latest processes
 echo "<br><b>Ten latest cpu intensive processes:</b><br><pre>" >> $TMP
@@ -60,6 +91,13 @@ echo "</pre>" >> $TMP
 echo "<br><b>Proc meminfo:</b><br><pre>" >> $TMP
 cat /proc/meminfo >> $TMP
 echo "</pre>" >> $TMP
+
+# /proc/swaps
+echo "<br><b>Swaps:</b><br><pre>" >> $TMP
+cat /proc/swaps >> $TMP
+echo "</pre>" >> $TMP
+
+
 
 # Close html
 echo "</p></body></html>" >> $TMP
