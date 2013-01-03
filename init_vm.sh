85e2a4f02b6a6fd028583c6d9c4fd9493cb686b2
culdev <lucas03093@gmail.com>
2013-01-03 15:01:18 +0100

    Typo

--- init_vm.sh
+++ init_vm.sh
@@ -186,7 +186,7 @@ dc_hide_mailname='true'
 dc_mailname_in_oh='true'
 dc_localdelivery='mail_spool'
 " > /etc/update-exim4.conf.conf
-    update-exim4.config -v
+    update-exim4.conf -v
     echo "Done."
     echo "Restarting Exim4..."
     /etc/init.d/exim4 restart
