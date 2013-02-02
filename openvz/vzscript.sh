0acffa3cf2257e3c4546ce23042cd7947eaa90de
culdev <lucas03093@gmail.com>
2013-02-02 11:03:13 +0100

    Merged bridgenetwork.sh and burstmemory.sh into a single script.

---
+++ openvz/vzscript.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# $1 = Variable to check for content
+isSet()
+{
+    if [ ! "$1" ] || [ "$1" == "" ]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+case "$1" in
+    # Bridge network
+    -b)
+        if ! isSet "$2"; then
+            echo "Requires container id."
+            exit 1
+        fi
+        
+        vzctl set $2 --netif_del eth0 --save
+        vzctl set $2 --netif_del eth1 --save
+        vzctl set $2 --netif_add eth0,,,,vzbr0 --save
+        vzctl set $2 --netif_add eth1,,,,vzbr1 --save
+        
+        echo "Done."
+    ;;
+    
+    # Burst memory
+    -bm)
+        ct=$2
+        guarmem=$3
+        burstmem=$4
+
+        if [ "$ct" == "" ] || [ "$guarmem" == "" ] || [ "$burstmem" == "" ]; then
+            echo -e "Missing parameters, 2 = container id, 3 = guaranteed memory, 4 = burst memory.\nE.g $0 -bm 100 256M 512M\nContainer id 100 will receive 256MB guaranteed memory and 512MB burstable."
+            exit 1
+        fi
+         
+        vzctl set $ct --vmguarpages $guarmem --save
+        vzctl set $ct --oomguarpages unlimited --save
+        vzctl set $ct --privvmpages $burstmem --save
+        vzctl set $ct --physpages unlimited --save
+    ;;
+
+    
+    # Display usage
+    *)
+    echo -e "Usage: $(basename $0) OPTION...
+
+-h                          Displays this text.
+-b {CTID}                   Removes eth0 and bridges eth0 and eth1 with host.
+-bm {CTID} {GM} {BM}        Adds burst memory to CTID.
+                             GM = Guaranteed memory
+                             BM = Burst memory"
+    ;;
+esac
+
+exit 0
