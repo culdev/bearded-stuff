578143aeb9235cb4bdfbaa03932d706208c8be64
culdev <lucas03093@gmail.com>
2013-02-02 14:13:32 +0100

    Added initiate.

--- openvz/vzscript.sh
+++ openvz/vzscript.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BASENAME=$(basename $0)
+
 # $1 = Variable to check for content
 isSet()
 {
@@ -42,18 +44,59 @@ case "$1" in
         vzctl set $ct --privvmpages $burstmem --save
         vzctl set $ct --physpages unlimited --save
     ;;
+    
+    # Initiate container
+    initiate)
+        ctid=$2
+        eth1ip=$3
+        
+        if ! isSet "$ctid" || ! isSet "$eth1ip"; then
+            echo "Missing parameters.
+Usage: $BASENAME {CTID} {eth1 IP}
+Example: $BASENAME 100 23
+Initiates container id 100 with eth1 ip 10.10.1.23"
+            exit 1
+        fi
+        
+        $0 bridge $ctid
+        vzctl start $ctid
+        
+        # Regenerate ssh keys
+        if [ "$(vzctl exec $ctid "dpkg --get-selections | grep openssh-server" | wc -l)" != "0" ]; then
+            vzctl exec $ctid rm -f /etc/ssh/ssh_host_*_key*
+            vzctl exec $ctid dpkg-reconfigure openssh-server
+        fi
+        
+        # Debian
+        if vzctl exec $ctid "cat /etc/debian_version" > /dev/null; then
+            # Setup eth1
+            vzctl exec $ctid "echo \"auto eth1
+iface eth1 inet static
+    address 10.10.1.$eth1ip
+    netmask 255.255.255.0
+    gateway 10.10.1.1\" >> /etc/network/interfaces"
+        else
+            echo "Only debian is supported for now."
+        fi
+        
+        vzctl restart $ctid
+        
+        echo "Initiated container $ctid."
+    ;;
 
     
     # Display usage
     *)
-    echo -e "Usage: $(basename $0) OPTION...
+    echo -e "Usage: $BASENAME OPTION...
 
 help                        Displays this text.
 bridge {CTID}               Removes eth0 and bridges eth0 and eth1 with host.
 burstmemory {CTID} {GM} {BM}
                             Adds burst memory to CTID.
                              GM = Guaranteed memory
-                             BM = Burst memory"
+                             BM = Burst memory
+initiate {CTID} {eth1 IP}   Bridges network connections, adds eth1 on Debian
+                             and regenerates ssh keys."
     ;;
 esac
 
