931ab5528693fc56ca845b4815cab07f0eeb1304
culdev <lucas03093@gmail.com>
2013-02-10 15:44:45 +0100

    Dhcpd leases sync script.

---
+++ raspberrypi/server/dhcpd_sync.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+FROM="/var/lib/dhcp/"
+TO="/var/lib/dhcp_persist/"
+
+# -v, --verbose               increase verbosity
+# -u, --update                skip files that are newer on the receiver
+# -r, --recursive             recurse into directories
+# --delete                    remove files from $TO if they do not exist in $FROM
+rsync -vur --delete $FROM $TO
